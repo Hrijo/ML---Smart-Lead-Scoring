dc61806ea3e8c62961528727344ea811727afbda
Hrijo <85121157+Hrijo@users.noreply.github.com>
2022-06-05 17:14:17 +0530

    Add files via upload

---
+++ Smart_lead_scoring.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_estimator as fc
+
+df_test = pd.read_csv('C:/Users/Hrishikesh/Desktop/Python/Leetcode Problems/Machine_learning/test_leads.csv')
+df_test.head()
