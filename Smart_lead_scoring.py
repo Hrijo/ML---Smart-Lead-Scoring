01ea84cfb7471a5a0287085d395588a70a29d910
Hrijo <85121157+Hrijo@users.noreply.github.com>
2022-08-06 19:42:55 +0530

    Delete Smart_lead_scoring.py

--- Smart_lead_scoring.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import tensorflow_estimator as fc
-
-df_test = pd.read_csv('C:/Users/Hrishikesh/Desktop/Python/Leetcode Problems/Machine_learning/test_leads.csv')
-df_test.head()
